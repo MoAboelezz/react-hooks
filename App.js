1a3a496fd4c2ff77d272367f5b8722a910e75a8f
MoAboelezz <133234365+MoAboelezz@users.noreply.github.com>
2023-12-31 14:51:39 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieList from './MovieList';
+function App() {
+  return (
+    <>
+    {/* <MovieCard/> */}
+    <MovieList />
+    </>
+  );
+}
+
+export default App;
